36058b5992924196683e6d0d2a53c98d88498800
Mustafa İlker Kamaci <mustafailkerkmc@gmail.com>
2025-06-02 15:20:36 +0300

    Add files via upload

---
+++ Encapsulation_Practice/Encapsulation_Practice/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Practice
+{
+    internal class Car
+    {
+        public string Brand { get; set; } // arabanin markasini tutan ozellik 
+        public string Model { get; set; } // arabanin modelini tutan ozellik
+        public string Color  { get; set; } // arabanin rengini tutan ozellik
+
+        private int _doors; // arabanin kapi sayisini  tutan ozellik, bu alana dogrudan erisilemez sadece sinif icersinden erisilebilir.    
+
+        public int Doors { get { return _doors; }
+            set // eger atanmak sistenen kapi sayisi 2 veya 4se _ doors alanini set kisminin icindekiler atanacak
+            {
+                if(value == 2 || value == 4)
+                {
+                    _doors = value;
+                }
+                else // eger 2 veya 4 disinda bir deger ise uyari mesaji verir ve _doorsu -1 olarak ayarlar.
+                {
+                    Console.WriteLine("!!!WARNING! The number of doors can only be 2 or 4.");
+                    _doors = -1;
+                }
+            }
+        }
+
+        // Constructor:
+
+        public Car(string brand, string model, string color, int doors)
+        {
+            Brand = brand; // gelen degerleri sinifin ozelliklerine atar.
+            Model = model;
+            Color = color;
+            Doors = doors; // set metodu cagrilir ve kapi sayisi kontrol edilir.
+        }
+
+        // arabanin bilgilerini yazmasi icin bir metot daha tanimliyorum.
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("-----Car Information-----");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Color: {Color}");
+            if (Doors == -1)
+            {
+                Console.WriteLine("WARNING! The number of doors can only be 2 or 4");
+            }
+            else
+            {
+                Console.WriteLine($"Doors: {Doors}");
+            }
+        }
+
+
+    }
+}
---
+++ Encapsulation_Practice/Encapsulation_Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Encapsulation_Practice;
+
+Car car1 = new Car("Toyota", "Corolla", "Red", 4);
+
+car1.ShowInfo();
+
+Car car2 = new Car("Honda", "Civic", "Blue", 2);
+car2.ShowInfo();
+
+Car car3 = new Car("Ford", "Mustang", "Black", 3); // Bu durumda uyarı mesajı alacağız.
+car3.ShowInfo(); // Bu durumda kapi sayisi -1 olarak ayarlanacak ve uyarı mesajı verilecek.
+
+//valoran bir arabinin kapi sayisini degistirmeye calistigimiza ise metota tanimladigimiz hata mesaji  gosterecek.
+
+car1.Doors = 3;
+car1.ShowInfo(); // Bu durumda kapi sayisi -1 olarak ayarlanacak ve uyarı mesajı verilecek.
